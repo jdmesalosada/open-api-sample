9e3b1093949f2f205937a30685d119a8bcf6756c
Julian Mesa Losada <julian.mesa@scotiabankcolpatria.com>
2022-04-17 18:26:20 -0500

    added info to readme file

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,4 +3,14 @@ function fn() {
         baseUrl: 'http://localhost:8080/home-energy'
     };
     return config;
+
+        /*karate.configure('connectTimeout', 10000);
+        karate.configure('readTimeout', 10000);
+
+        var config = {
+            icndbBaseUrl: 'http://api.icndb.com/jokes/',
+            weatherBaseUrl: 'http://api.openweathermap.org/'
+        };
+        return config;*/
+
 }
